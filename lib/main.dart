8f9f3fedd4ef0732ab84c05c47127bed4436c85a
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-01-22 13:50:35 +0200

    create adaptive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:responsive_dash_board/features/dashboard/presentation/view/dash_board_view.dart';
 
 void main() {
   runApp(DevicePreview(
@@ -8,92 +8,37 @@ void main() {
       tools: const [
         ...DevicePreview.defaultTools,
       ],
-      builder: (context) => const MyApp()));
+      builder: (context) => const AdminDashBoard()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AdminDashBoard extends StatelessWidget {
+  const AdminDashBoard({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashBoardView(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  double getResponsiveFontSize(BuildContext context,
+      {required double fontSize}) {
+    double scaleFactor = getScaleFactor(context);
+    double responsiveFontSize = fontSize * scaleFactor;
+    double lowerLimit = fontSize * .8;
+    double upperLimit = fontSize * 1.2;
+    return responsiveFontSize.clamp(lowerLimit, upperLimit);
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
-        //
-        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-        // action in the IDE, or press "p" in the console), to see the
-        // wireframe for each widget.
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'You have pushed the button this many times:',
-            style: TextStyle(fontSize: 30.w),
-          ),
-          const Text(
-            "my name mahmoud saber",
-            style: TextStyle(fontSize: 20),
-          ),
-        ],
-      ),
-    );
+  double getScaleFactor(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    if (width < 600) {
+      return width / 400;
+    } else if (width < 900) {
+      return width / 700;
+    } else {
+      return width / 1000;
+    }
   }
 }
