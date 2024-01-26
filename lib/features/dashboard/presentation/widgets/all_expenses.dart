b559412c001d1bbea62f70b9fcaea3782a7e74d7
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-01-26 22:25:13 +0200

    create custom background container

--- lib/features/dashboard/presentation/widgets/all_expenses.dart
+++ lib/features/dashboard/presentation/widgets/all_expenses.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/core/utils/functions/app_images.dart';
+import 'package:responsive_dash_board/core/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/features/dashboard/presentation/widgets/all_expensess_header.dart';
-import 'package:responsive_dash_board/features/dashboard/presentation/widgets/all_expensess_item.dart';
 import 'package:responsive_dash_board/features/dashboard/presentation/widgets/all_expensess_item_list_view.dart';
-import 'package:responsive_dash_board/models/all_expensess_item_model.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      padding: 20,
+      child: Column(
         children: [
           AllExpensessHeader(),
           SizedBox(
